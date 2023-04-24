72119b565428a8733980b90d7a13c8fbe970e9c5
José Augusto <joseaugustogomessantos12@gmail.com>
2023-04-24 19:47:18 -0300

    Função para botão da nav

--- script.js
+++ script.js
@@ -15,3 +15,25 @@ function myFunction() {
       }
     }
   }
+
+
+  
+  
+
+  function Function() {
+    document.getElementById("Dropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
